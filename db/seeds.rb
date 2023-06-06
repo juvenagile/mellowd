1d031627ed63c4e319b80ab3070b1e365f1ed911
Damian Reyes <reyesmezadamian2@gmail.com>
2023-06-06 17:27:58 -0600

    pain in the ass but done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 GENRE = %w[rock pop urban dance/dj ballads tropical regional country instrumental choir].freeze
+user = User.create(email: "33@gmail.com" , password: "12345678", first_name: "Juan", last_name: "Diaz", admin: false)
+user_2 = User.create(email: "23@gmail.com" , password: "123456", first_name: "John", last_name: "Doe", admin: true)
 
+all_user = user, user_2
 25.times do
   Event.create(
     title: "Title #{rand(1..100)}",
@@ -7,6 +10,7 @@ GENRE = %w[rock pop urban dance/dj ballads tropical regional country instrumenta
     date: Date.today + rand(1..180),
     duration: rand(30..120),
     genre: GENRE.sample,
+    user_id: all_user.sample.id,
     location: ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"].sample
   )
   puts 'Done'
